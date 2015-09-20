f2a1ea86fbe1af464eeb3537ed3e98aa498ecf66
Alessandro <thedragonite@gmail.com>
2015-09-20 18:28:56 +0200

    Push Apple Music Festival 2015 change.

--- itunes-festival.sh
+++ itunes-festival.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-# iTunes Festival 2014 - Show Downloader
+# Apple Music Festival 2015 - Show Downloader
 # Variables 
-cookie="token=$(curl -s http://itunes.apple.com/apps1b/authtoken/token.txt)"
+cookie="token=$(curl -s https://itunes.apple.com/apps1b/authtoken/token.txt)"
 tag=$1
 artist=$2
 counter=1
-output=${artist}_itf${tag}092014.ts
+output=${artist}_amf${tag}092015.ts
 
 # Making temp directory
 mkdir -p $artist
@@ -20,19 +20,17 @@ idartist=$3
 fi
 
 # Fetching parts links
-     echo "iTunes Festival 2014 - Start download of ${artist} show ($tag/09/2014)"
-index=$(curl -s -b "$cookie" http://streaming.itunesfestival.com/auth/eu6/vod/201409$tag/v1/${idartist}_${artist}_desktop_vod.m3u8 | tail -n1)
-
-# Check if the input data are right.
-if [[ $index =~ "</Message></Error>" ]]; then
+     echo "Apple Music Festival 2015 - Start download of ${artist} show ($tag/09/2015)"
+# Fetching parts links
+files2=$(curl -s --cookie "$cookie" "http://streaming.itunesfestival.com/auth/eu1/vod/201509$tag/v1/8500_256/{$idartist}_${artist}_vod.m3u8")
+files=$(echo $files2 | tr -d '\r')
+if [[ $files =~ "</Message></Error>" ]]; then
     echo "Error! Wrong day or artist name. Check the input parameters and remember: NO SPACE IN ARTIST NAME!"
     cd ..
     rm -r $artist
     exit 1;
 fi
 
-# Fetching parts links
-files=$(curl -s -b "$cookie" http://streaming.itunesfestival.com/auth/eu6/vod/201409$tag/v1/$index)
 items=$(grep -o song <<< ${files[*]} | wc -l | tr -d ' ')
 
 # Download parts
@@ -42,7 +40,8 @@ for i in $files; do
       if [[ ! -f $counter.ts ]]; then
          echo "Downloading part $counter of $items"
          # Check download status and exit if fail
-         until curl -m 600 -b "$cookie" http://streaming.itunesfestival.com/auth/eu6/vod/201409$tag/v1/8500_256/$i > $counter.ts; do
+         #cat $i
+         until curl -m 600 --cookie "${cookie}" http://streaming.itunesfestival.com/auth/eu1/vod/201509$tag/v1/8500_256/$i > $counter.ts; do
          rm -f $counter.ts
          echo "Failed to download. Try re-run the script for resume."
          exit 1;
